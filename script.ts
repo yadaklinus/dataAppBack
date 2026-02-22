8745c237002441d84bda1061728542cf35782fac
CodeGit <linusyadak@gmail.com>
2026-02-23 00:20:29 +0100

    fix2

--- script.ts
+++ script.ts
@@ -91,7 +91,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 
 // 10. Start Background Services
 // Uncommented because this is your safety net for failed webhooks!
-//startTransactionSync();
+startTransactionSync();
 startMonnifyTransactionSync()
 
 app.listen(PORT, () => {
