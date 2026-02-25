5268c4e1307373a85451fb4946078899a6617a13
CodeGit <linusyadak@gmail.com>
2026-02-25 18:29:10 +0100

    nodist

--- dist/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require('express');
-const router = express.Router();
-const adminController = require('@/api/v1/admin/adminController');
-const { authorizeAdmin } = require('@/middleware/authMiddleware');
-// Administrative Stats - Restricted to ADMIN tier
-router.get('/stats', adminController.getGeneralStats);
-module.exports = router;
